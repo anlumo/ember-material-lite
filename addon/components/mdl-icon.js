b2f3a7e39ae64f0f248ddde4e637aa2d8378e0ca
Andreas Monitzer <andreas@monitzer.com>
2018-04-12 00:28:36 +0200

    Removed icon class from mdl-icon, since that's not required for material icons and can have unintentional side effects when this class name is used for something else.

--- addon/components/mdl-icon.js
+++ addon/components/mdl-icon.js
@@ -3,6 +3,6 @@ import BadgeSupport from '../mixins/badge-support';
 import layout from '../templates/components/mdl-icon';
 
 export default Component.extend(BadgeSupport, {
-  classNames: ['icon', 'material-icons'],
+  classNames: ['material-icons'],
   layout
 });
