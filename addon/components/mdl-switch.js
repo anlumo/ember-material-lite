04b7439cfaa1ed0d3ade049e4586b2b5edee943f
Andreas Monitzer <andreas@monitzer.com>
2018-04-15 05:02:47 +0200

    Update switch when its value changes.

--- addon/components/mdl-switch.js
+++ addon/components/mdl-switch.js
@@ -1,5 +1,7 @@
 import BaseToggleComponent from './-base-toggle-component';
 import layout from '../templates/components/mdl-switch';
+import { observer } from '@ember/object';
+import { schedule } from '@ember/runloop';
 
 export default BaseToggleComponent.extend({
   primaryClassName: 'switch',
@@ -8,5 +10,9 @@ export default BaseToggleComponent.extend({
     this._super(...arguments);
     let mdlswitch = new window.MaterialSwitch(this.get('element'));
     this.set('_mdlComponent', mdlswitch);
-  }
+  },
+
+  updateValue: observer('_mdlComponent', 'value', function() {
+    schedule('afterRender', this, () => this.get('_mdlComponent').checkToggleState());
+  }),
 });
